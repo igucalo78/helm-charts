41ec0f320b4dd4e206320bd2d53a0e45209342d4
Ad <ad@accelleran.com>
2020-04-14 20:26:11 +0000

    Updated visuals of update_repo.sh

--- update_repo.sh
+++ update_repo.sh
@@ -7,49 +7,49 @@
 
 
 # helm package verything except acc-dashboard
-echo "Packaging everything except acc-dashboard"
+echo "##### Packaging everything except acc-dashboard"
 find -maxdepth 1 -mindepth 1 \( \! -name "acc-dashboard" \! -name ".git" \! -name "*.tgz" \! -name "index.yaml" \! -name "*.sh*" \! -name "" \! -name "beta" \! -name "CHANGELOG.md" \! -name "README.md" \) -exec sh -c '
  for i do
    helm package "$i"
  done' sh {} +
 
-echo "Creating index.yaml for everything except acc-dashboard"
+echo "##### Creating index.yaml for everything except acc-dashboard"
 # Create index yaml
 helm repo index . --url  https://accelleran.github.io/helm-charts/
 
-echo "Adding  everything excpet acc-dashboard"
+echo "##### Adding  everything excpet acc-dashboard"
 #git add all except acc-dashboard
 find -maxdepth 1 -mindepth 1 \( \! -name "acc-dashboard*" \! -name ".git" \! -name "" \) -exec sh -c '
   for i do
     git add "$i"
   done' sh {} +
 
-echo "Commiting everything excpet acc-dashboard"
+echo "##### Commiting everything excpet acc-dashboard"
 git commit -m "$1" --author="$2"
 
-echo "Pushing everything excpet acc-dashboard"
+echo "##### Pushing everything excpet acc-dashboard"
 git push
 
-echo "Sleeping for 30 seconds..."
+echo "##### Sleeping for 30 seconds..."
 sleep 30
 
-echo "Updating helm chart repo"
+echo "##### Updating helm chart repo"
 helm repo update
 
-echo "Updating the dependencies of acc-dashboard"
+echo "##### Updating the dependencies of acc-dashboard"
 cd acc-dashboard && helm dependency update && cd ..
 
-echo "Pacakge acc-dashboard"
+echo "##### Pacakge acc-dashboard"
 helm package acc-dashboard
 
-echo "Update index.yaml"
+echo "##### Update index.yaml"
 helm repo index . --url  https://accelleran.github.io/helm-charts/
 
-echo "Adding"
+echo "##### Adding"
 git add acc-dashboard* index.yaml
 
-echo "Commiting"
+echo "##### Commiting"
 git commit -m "$1" --author="$2"
 
-echo "Pushing"
+echo "##### Pushing"
 git push
