4751104525c6fdf9a6d0f54e038344173319b8da
ad <ad@accelleran.com>
2020-06-19 15:57:22 +0000

    Whole bunch of beta charts

---
+++ beta/restart_l2s.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ssh -i ~/e1010_accelleran.txt accelleran@10.22.10.140 'reboot'
+ssh -i ~/e1010_accelleran.txt accelleran@10.22.10.148 'reboot'
+ssh -i ~/e1010_accelleran.txt accelleran@10.22.10.33 'reboot'
+ssh -i ~/e1010_accelleran.txt accelleran@10.22.10.34 'reboot'
+ssh -i ~/e1010_accelleran.txt accelleran@10.22.10.51 'reboot'
+ssh -i ~/e1010_accelleran.txt accelleran@10.22.10.29 'reboot'
+
