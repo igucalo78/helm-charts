99b9fbe7f73c9878988974de5e92984cd8bcf153
ad <ad@accelleran.com>
2020-08-11 08:01:39 +0000

    Deleting beta folder on master

--- beta/restart_l2s.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ssh -i ~/e1010_accelleran.txt accelleran@10.22.10.140 'reboot'
-ssh -i ~/e1010_accelleran.txt accelleran@10.22.10.148 'reboot'
-ssh -i ~/e1010_accelleran.txt accelleran@10.22.10.33 'reboot'
-ssh -i ~/e1010_accelleran.txt accelleran@10.22.10.34 'reboot'
-ssh -i ~/e1010_accelleran.txt accelleran@10.22.10.51 'reboot'
-ssh -i ~/e1010_accelleran.txt accelleran@10.22.10.29 'reboot'
-
