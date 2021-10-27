dcb2d6fd8eefa23d7573b11a79c93f1f0cd2fcf0
Ensar Zeljkovic <ensar.zeljkovic@uantwerpen.be>
2021-10-27 11:28:24 +0200

    adding APPID as seperate value in values file updating script to deploy multiple e2-aps

--- e2t/scripts/deploy_e2t_aps.sh
+++ e2t/scripts/deploy_e2t_aps.sh
@@ -7,8 +7,8 @@ echo $i
 
 if $DRY_RUN
 then
-helm install e2-ap-$i ~/helm-charts/e2t-ap --values ~/values_files/e2t-ap_0-2-0.yaml --set-string bootstrapId=e2-ap-$i --dry-run --debug
+helm install e2-ap-$i ~/helm-charts/e2t-ap --values ~/values_files/e2t-ap_0-2-1.yaml --set-string appId=e2-ap-$i --dry-run --debug
 else
-helm install e2-ap-$i ~/helm-charts/e2t-ap --values ~/values_files/e2t-ap_0-2-0.yaml --set-string bootstrapId=e2-ap-$i
+helm install e2-ap-$i ~/helm-charts/e2t-ap --values ~/values_files/e2t-ap_0-2-1.yaml --set-string appId=e2-ap-$i
 fi
 done
